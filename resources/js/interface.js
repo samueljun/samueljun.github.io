a1296872e13091f851b92a23928c25607763dab0
Samuel Jun <sjun@novacoast.com>
2013-08-25 15:26:43 -0700

    Improved hash navigation

--- resources/js/interface.js
+++ resources/js/interface.js
@@ -1,7 +1,13 @@
-$(document).ready(function() {
+function loadTab() {
 	var hash = window.location.hash;
-	if (hash != undefined)
-		$(hash).tab("show");
+	if (hash != "")
+		$("a[href=\"" + hash + "\"]").tab("show");
+}
+
+$(document).ready(function() {
+	loadTab();
+
+	$(window).on("hashchange", loadTab);
 
 	$(".navbar a").click(function (e) {
 		$(this).tab("show");

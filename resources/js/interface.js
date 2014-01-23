8da8e71ccb35c50a0c8f7ff7498c25a9e18c1ef2
Samuel Jun <samuel.h.jun@gmail.com>
2014-01-23 01:11:02 -0800

    Removed #home has for main page

--- resources/js/interface.js
+++ resources/js/interface.js
@@ -1,15 +1,11 @@
 function loadTab() {
 	var hash = window.location.hash;
-	if (hash != "")
-		$("a[href=\"" + hash + "\"]").tab("show");
+	if (hash != "") {
+		$("a[href='" + hash + "']").tab("show");
+	}
 }
 
 $(document).ready(function() {
 	loadTab();
-
 	$(window).on("hashchange", loadTab);
-
-	$(".navbar a").click(function (e) {
-		$(this).tab("show");
-	});
 });

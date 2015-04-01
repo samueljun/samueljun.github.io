6ca90dc1e556600e0d2e2723dd1f946aa95520c9
Samuel Jun <mrsamueljun@gmail.com>
2015-04-01 12:28:41 -0700

    Updated email and added birthday javascript

--- resources/js/interface.js
+++ resources/js/interface.js
@@ -5,7 +5,15 @@ function loadTab() {
 	}
 }
 
+// http://stackoverflow.com/questions/4060004/calculate-age-in-javascript
+function _calculateAge(birthday) { // birthday is a date
+	var ageDifMs = Date.now() - birthday.getTime();
+	var ageDate = new Date(ageDifMs); // miliseconds from epoch
+	return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
 $(document).ready(function() {
 	loadTab();
 	$(window).on("hashchange", loadTab);
+	$('#age').html(_calculateAge(new Date('5/8/92')));
 });

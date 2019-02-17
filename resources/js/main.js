ff4400644ee1fae8c8a61653c2d57b3dd94804bd
Samuel Jun <mrsamueljun@gmail.com>
2019-02-17 02:14:04 -0800

    Update Pomodoro Clock to Tomato Clock

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,19 +1,19 @@
 function loadTab() {
-	var hash = window.location.hash;
-	if (hash != "") {
-		$("a[href='" + hash + "']").tab("show");
-	}
+  var hash = window.location.hash;
+  if (hash != "") {
+    $("a[href='" + hash + "']").tab("show");
+  }
 }
 
 // http://stackoverflow.com/questions/4060004/calculate-age-in-javascript
-function _calculateAge(birthday) { // birthday is a date
-	var ageDifMs = Date.now() - birthday.getTime();
-	var ageDate = new Date(ageDifMs); // miliseconds from epoch
-	return Math.abs(ageDate.getUTCFullYear() - 1970);
+function _calculateAge(birthday) {
+  // birthday is a date
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
 $(document).ready(function() {
-	loadTab();
-	$(window).on("hashchange", loadTab);
-	$('#age').html(_calculateAge(new Date('5/8/92')));
+  loadTab();
+  $(window).on("hashchange", loadTab);
 });
